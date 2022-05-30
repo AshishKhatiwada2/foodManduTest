1c039561ce3f046def69fb278dd55e15747a6597
ashish khatiwada <ashishkhatiwada212@gmail.com>
2022-05-30 14:33:01 +0545

    Changed the Project to POM Model. Also added Negative test with Incorrect Input parameter. Still needed to add Extent Report.

--- src/test/java/com/example/seleniumtry3/TestBase.java
+++ src/test/java/com/example/seleniumtry3/TestBase.java
@@ -2,33 +2,3 @@ package com.example.seleniumtry3;
 
 public class TestBase {
 }
-
-//package io.automatenow.tests;
-//
-//        import io.automatenow.pages.*;
-//        import org.testng.Assert;
-//        import org.testng.annotations.*;
-
-//
-//public class TestBase extends BasePage {
-//    protected NavigationBar navBar;
-//    protected HomePage homePage;
-//    protected SandboxPage sandboxPage;
-//
-//
-//    @BeforeMethod(alwaysRun = true)
-//    @Parameters("browser")
-//    public void setup(@Optional("chrome") String browser) {
-//        navBar = new NavigationBar();
-//        homePage = new HomePage();
-//        sandboxPage = new SandboxPage();
-//
-//        Assert.assertTrue(goToHomepage(browser), "An error occurred while navigating to the homepage");
-//    }
-//
-//
-//    @AfterMethod(alwaysRun = true)
-//    public void tearDown() {
-//        closeBrowser();
-//    }
-//}
